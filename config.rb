b7bfa2be5c9df23183bc21b7693dac1b0b4a5a40
FearGoidte <FearGoidte@users.noreply.github.com>
2016-06-14 16:29:23 +0100

    Added Timezone data for dates

--- config.rb
+++ config.rb
@@ -1,18 +1,5 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page '/this-page-has-no-layout.html', layout: false
-
-# With alternative layout
-# page '/path/to/file.html', layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy '/this-page-has-no-template.html', '/template-file.html', locals: {
-#  which_fake_page: 'Rendering a fake page with a local variable' }
+# Timezone is required to add the timezone to dates
+Time.zone = 'London'
 
 ###
 # Helpers
@@ -21,7 +8,7 @@
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = 'blog'
-  blog.permalink = '{year}/{month}/{day}/{title}.xhtml'
+  blog.permalink = '{category}/{title}.xhtml'
   # Matcher for blog source files
   blog.sources = 'articles/{year}-{month}-{day}-{title}.xhtml'
   blog.taglink = 'tags/{tag}.xhtml'
@@ -48,14 +35,7 @@ set :trailing_slash, false
 
 # Reload the browser automatically whenever files change
 # configure :development do
-#   activate :livereload
-# end
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
+#
 # end
 
 # Development-specific configuration
