93e36cd2541988c42e9deb2dcc27d10c9e6f8cf4
FearGoidte <FearGoidte@users.noreply.github.com>
2016-06-15 08:21:35 +0100

    Simplified doc_title helper

--- helpers/doc_title.rb
+++ helpers/doc_title.rb
@@ -2,13 +2,10 @@ def doc_title
   seperator = '—'
   site_name = data.settings.site.name
   if @title
-    page_title = @title
+    @title + seperator + site_name
   elsif current_resource.data.title
-    page_title = current_resource.data.title
-  end
-  if page_title
-    page_title + seperator + site_name
+    current_resource.data.title + seperator + site_name
   else
-    return site_name
+    site_name
   end
 end
