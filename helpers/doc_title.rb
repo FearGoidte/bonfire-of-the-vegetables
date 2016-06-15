5deb1621a9144c5dfd34509b762579c3c230954b
FearGoidte <FearGoidte@users.noreply.github.com>
2016-06-15 10:06:24 +0100

    Corrected embarrassing spelling mistake

--- helpers/doc_title.rb
+++ helpers/doc_title.rb
@@ -1,10 +1,10 @@
 def doc_title
-  seperator = '—'
+  separator = '—'
   site_name = data.settings.site.name
   if @title
-    @title + seperator + site_name
+    @title + separator + site_name
   elsif current_resource.data.title
-    current_resource.data.title + seperator + site_name
+    current_resource.data.title + separator + site_name
   else
     site_name
   end
